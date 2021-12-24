329af5afe9a17cc3f22eff188c307ced1ed9c5a7
Megan McLean <425megs@gmail.com>
2021-12-23 19:14:36 -0800

    comments added to code

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
-// press generate password --> prompt for length --> prompt for uppercase -->prompt for lowercase --> promt for special characters --> password is displayed
+// Array for uppercase options
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+// Array for lowercase options
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-// Assignment Code -- button
+// Array for special charater options
+var specs = ["!", ",", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~"]
+
+// Array for number options
+var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// Button
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input -- function to be triggered
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// need an empty array where password will live 
+// Empty array where options will be concacted 
 var mainArray = []
+
+// Empty string where password will be displayed after randomization
 var passwordString = ""
 
-// something like... 
-// if 'okay' for uppercase ... then generate a random letter between A-Z
-//   else ... ?
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-// if 'okay' for lowercase ... then generate a random letter between a-z
-//   else ... ?
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-// if 'okay' for special characters ... then randomly select from "~!@#$%^&*()-+<>"
-//   else ...?
-var specs = ["!", ",", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "{", "|", "}", "~"]
-// if 'okay' for numbers ... randomly select number between 0-9
-var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-// How will I know how many letters vs numbers vs special characters to include?
-
-
-
+// Function for various prompts, selector and randomizer
 function generatePassword() {
-  var length = prompt("How long do you want your password to be? Select a number between 8-128");
-  // console.log(length)
+  var length = prompt ("How long do you want your password to be? Select a number between 8-128");
+  while(length < 8 || length > 128) {
+    length = prompt("Please select a value between 8-128");
+  }
   var addUpperCase = confirm("Do you want to include uppercase letters?")
-  // console.log(upperCase)
   var addLowerCase = confirm("Do you want to include lowercase letters?")
-  // console.log(lowerCase)
   var addNumbers = confirm("Do you want to include numbers?")
-  // console.log(numbers)
   var addSpecialChar = confirm("Do you want to include special characters?")
-  // console.log(specialChar)
 
   if (addUpperCase === true) {
     mainArray = mainArray.concat(upper);
@@ -64,9 +57,7 @@ function generatePassword() {
   }
 
   return passwordString;
-  // console.log(mainArray)
 }
 
-// Add event listener to generate button -- trigger to initiate function
+// Trigger to initiate function
 generateBtn.addEventListener("click", writePassword);
-
